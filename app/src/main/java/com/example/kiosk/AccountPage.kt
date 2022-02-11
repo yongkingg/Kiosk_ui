420d36261301924f7fbaa64fbddd6ccb7e590e4d
yongkingg <dydwns8064@naver.com>
2022-02-11 20:47:15 +0900

    ui fetch

--- app/src/main/java/com/example/kiosk/AccountPage.kt
+++ app/src/main/java/com/example/kiosk/AccountPage.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 
 class AccountPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.backbtn_fragment)
         var headText = findViewById<TextView>(R.id.textView)
+        var head = findViewById<ConstraintLayout>(R.id.linearLayout)
         headText.setText("로그인")
         supportFragmentManager.beginTransaction().replace(R.id.mainlayout,AccountPageBody()).commit()
 
